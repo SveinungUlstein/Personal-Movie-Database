2e6d4e9db28f86ca67415d7b0e03bf640357dee0
Sveinung <sveinung.ulstein@hotmail.com>
2025-11-29 20:02:15 +0100

    Big push. Added background, seach bar , utils and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as React from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 interface Movie {
   id: number;
@@ -30,15 +31,30 @@ export default function App() {
 
   return (
     <div>
-      <h1>Popular Movies</h1>
-      <ul className="flex flex-row, overflow-x-auto gap-8">
-        {movies.map((movie) => (
-          <li className="flex-shrink-0 w-1/6 max-w-50 min-w-30">
-            <img id="movie_poster" className="rounded-lg hover:scale-105" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <div className="">{movie.title} ({movie.release_date})</div>
-          </li>
-        ))}
-      </ul>
+      <AuroraBackground showRadialGradient={true} className="bg-fixed inset-0 relative">
+        <div className="absolute top-32">
+          <input type="text" id="search" placeholder="" 
+          className="relative z-20 bg-slate-500/20 rounded-2xl py-1.5 pr-3 pl-3 outline-none text-gray-300 backdrop-blur-none w-64 focus:w-80 focus:outline-none focus:bg-slate-400/25 transition-all duration-500"/>
+        </div>
+        <div className="absolute inset-x-20 inset-y-96">
+          <div className="">
+            <h1 className="text-4xl font-bold text-white text-left mb-2 ml-24">Popular Movies</h1>
+          </div>
+          <div className="bg-teal-950/0 backdrop-blur-none mx-24 z-10">
+            <ul className="flex flex-row overflow-x-auto gap-8 scrollbar scrollbar-track-rounded-full scrollbar scrollbar-thumb-slate-700 scrollbar-track-slate-800">
+              {movies.map((movie) => (
+                <li key={movie.id} className="flex-shrink-0 max-w-44 min-w-30 group">
+                  <img id="movie_poster" className="rounded-lg hover:cursor-pointer" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                  <div className="text-white group-hover:text-teal-600 hover:cursor-pointer">{movie.title} ({movie.release_date.slice(0,4)})</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </AuroraBackground>
+      <div>
+        <h1 className="text-9xl">Text</h1>
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,52 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [addVariablesForColors,
+    require('tailwind-scrollbar')
+  ],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
+
+// Helper to flatten color palette
+function flattenColorPalette(colors) {
+  return Object.assign(
+    {},
+    ...Object.entries(colors ?? {}).flatMap(([color, values]) =>
+      typeof values == "object"
+        ? Object.entries(flattenColorPalette(values)).map(([number, hex]) => ({
+            [number ? `${color}-${number}` : color]: hex,
+          }))
+        : [{ [`${color}`]: values }]
+    )
+  );
+}
+
